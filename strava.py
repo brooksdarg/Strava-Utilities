706e583497472d8b0825c2f4054b13c06c597911
Brooks <brooks@triton.(none)>
2011-09-06 18:12:56 -0700

    Added:   - gzip compression support for all HTTP requests   - support for fetching KOMs for an athlete   - stream support for segments and rides
    Removed:
      - memcache support

--- strava.py
+++ strava.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python
+# Python client library for Strava.com API v1
 # Brooks Sizemore, brooks@darg.net
 
+import gzip
 import json
-import memcache
+import re
 import urllib2
+
+from StringIO import StringIO
 
 class StravaObject(object):
   """Abstract Strava object."""
 
-  def __init__(self, memcache_servers=['127.0.0.1:11211']):
+  def __init__(self):
     """Default constructor."""
     self._api_base = 'http://app.strava.com/api/v1'
-    if memcache_servers:
-      self._InitMemcache(memcache_servers)
-
-  def _InitMemcache(self, servers):
-    """Initialize memcache instance."""
-    self._mc = memcache.Client(servers, debug=0)
 
   def GetId(self):
     """Returns the id of the object."""
@@ -25,23 +23,30 @@ class StravaObject(object):
   def GetName(self):
     """Returns the name of the object."""
     return self._name
+
+  def FetchViaHttp(self, url):
+    """Fetches a file/page over HTTP."""
+
+    # Create a new request with gzip encoding enabled
+    request = urllib2.Request(url)
+    request.add_header('Accept-Encoding', 'gzip')
+    response = urllib2.urlopen(request)
+
+    # Check to be sure the response came back encoded
+    if response.info().get('Content-Encoding') == 'gzip':
+      buf = StringIO(response.read())
+      f = gzip.GzipFile(fileobj=buf)
+      content = f.read()
+      f.close()
+    else:
+      content = response.read()
+
+    return content
   
-  def FetchJson(self, url, key=None):
+  def FetchJson(self, url):
     """Fetches a JSON object from Strava with caching."""
     
-    # Check memcache first
-    if self._mc and key:
-      obj_str = self._mc.get(key)
-      if obj_str:
-        return json.loads(obj_str)
-
-    f = urllib2.urlopen(url)
-    json_str = f.read()
-    f.close()
-
-    if self._mc and key:
-      self._mc.set(key, json_str)
-
+    json_str = self.FetchViaHttp(url)
     return json.loads(json_str)
 
   @classmethod
@@ -60,19 +65,17 @@ class Segment(StravaObject):
     self._id = id
     self._name = name
     self._url = '%s/segments/%d' % (self._api_base, int(self._id))
-    self._mc_key = '/segment/%d' % int(id)
 
   def Refresh(self):
     """Refreshes the object from Strava data."""
-    segment_json = self.FetchJson(self._url, key=self._mc_key)['segment']
+    segment_json = self.FetchJson(self._url)['segment']
     self._id = segment_json[u'id']
     self._name = segment_json[u'name']
 
   def GetEfforts(self):
     """Fetches the best efforts for a segment."""
     efforts_url = '%s/segments/%d/efforts?best=true' % (self._api_base, int(self._id))
-    mc_key = '/segment/%d/efforts' % int(self._id)
-    efforts_json = self.FetchJson(efforts_url, key=mc_key)
+    efforts_json = self.FetchJson(efforts_url)
 
     efforts = []
     rank = 1
@@ -94,6 +97,12 @@ class Segment(StravaObject):
     """Returns the KOM/leader athlete for the segment."""
     efforts = self.GetEfforts()
     return efforts[0].GetAthlete()
+
+  def GetStream(self):
+    """Returns the stream for the segment."""
+    stream_url = '%s/stream/segments/%d' % (self._api_base, int(self._id))
+    stream_json = self.FetchJson(stream_url)
+    return stream_json
 
 
 class Effort(StravaObject):
@@ -135,8 +144,7 @@ class Ride(StravaObject):
   def GetEfforts(self):
     """Fetches a list of efforts for a ride."""
     efforts_url = '%s/rides/%d/efforts' % (self._api_base, int(self._id))
-    mc_key = '/ride/%d/efforts' % int(self._id)
-    efforts_json = self.FetchJson(efforts_url, key=mc_key)
+    efforts_json = self.FetchJson(efforts_url)
 
     efforts = []
     for effort in efforts_json[u'efforts']:
@@ -147,8 +155,7 @@ class Ride(StravaObject):
   def GetStream(self):
     """Fetches the stream (coords over time) for a ride."""
     stream_url = '%s/streams/%d' % (self._api_base, int(self._id))
-    mc_key = '/ride/%d/stream' % int(self._id)
-    stream_json = self.FetchJson(stream_url, key=mc_key)
+    stream_json = self.FetchJson(stream_url)
     return stream_json
 
   def Refresh(self):
@@ -175,11 +182,30 @@ class Athlete(StravaObject):
   def GetRides(self):
     """Fetch a list of rides for the athlete."""
     rides_url = '%s/rides?athleteId=%d' % (self._api_base, int(self._id))
-    mc_key = '/rides/%d' % int(self._id)
-    rides_json = self.FetchJson(rides_url, key=mc_key)
+    rides_json = self.FetchJson(rides_url)
 
     rides = []
     for ride in rides_json[u'rides']:
       rides.append(Ride(ride[u'id'], name=ride[u'name']))
     return rides
+
+  def GetKOMs(self):
+    """Fetches a list of segments where Athlete is the leader."""
+
+    segments = []
+
+    for page_num in range(1, 999): # NO one has 999 pages of KOMs, right?
+      html = self.FetchViaHttp('http://www.strava.com/athletes/%d/segments/leader?page=%d' % (self._id, page_num))
+      matches = re.finditer('"/segments/(\d+)"\s+title="([^"]+)"', html)
+
+      num_matches = 0
+      for match in matches:
+        segment = Segment(int(match.group(1)), name=match.group(2))
+        segments.append(segment)
+        num_matches += 1
+
+      if num_matches == 0:
+        break
+
+    return segments
 
